d5fd279e3bd029aec1d9ad172e5e93e0e79d01b4
SolidifiedRay <ray2147483647@outlook.com>
2019-11-19 17:47:33 -0500

    Week 11

--- Maximum Square.py
+++ Maximum Square.py
@@ -2,6 +2,22 @@
 '''
 Maximum Square
 
+Ujan decided to make a new wooden roof for the house. He has n rectangular planks numbered from 1 to n. The i-th plank has size ai×1 (that is, the width is 1 and the height is ai).
+
+Now, Ujan wants to make a square roof. He will first choose some of the planks and place them side by side in some order. Then he will glue together all of these planks by their vertical sides. Finally, he will cut out a square from the resulting shape in such a way that the sides of the square are horizontal and vertical.
+
+For example, if Ujan had planks with lengths 4, 3, 1, 4 and 5, he could choose planks with lengths 4, 3 and 5. Then he can cut out a 3×3 square, which is the maximum possible. Note that this is not the only way he can obtain a 3×3 square.
+
+
+What is the maximum side length of the square Ujan can get?
+
+Input
+The first line of input contains a single integer k (1≤k≤10), the number of test cases in the input.
+
+For each test case, the first line contains a single integer n (1≤n≤1000), the number of planks Ujan has in store. The next line contains n integers a1,…,an (1≤ai≤n), the lengths of the planks.
+
+Output
+For each of the test cases, output a single integer, the maximum possible side length of the square.
 '''
 cases = input()
 output = []
